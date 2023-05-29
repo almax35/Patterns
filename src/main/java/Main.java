6274f34a84287310e2f42bd9d3cefee90faac92c
Kirill Babich <gonertheowner@gmail.com>
2023-05-29 23:55:46 +0300

    added log file and client logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ public class Main {
         Department department = new Department();
         RectalProxy rectalProxy = new RectalProxy();
         department.addSubscriber(rectalProxy);
+        department.addStudent(new Student("Ivanov", 3.5));
+        department.addStudent(new Student("Student N", 2.0));
         department.notifySubscribers();
         rectalProxy.checkStudents();
     }
