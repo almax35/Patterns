3df77e4810519b196fd9a3b0690b9252307b80ab
Kirill Babich <gonertheowner@gmail.com>
2023-05-29 20:25:00 +0300

    create student entity

---
+++ src/main/java/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String name;
+    private double averageScore;
+
+    public Student(String name, double averageScore) {
+        this.name = name;
+        this.averageScore = averageScore;
+    }
+}
