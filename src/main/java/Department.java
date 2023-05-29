706ce6bcfde8a6a016f69c8ed39f87df8e5e54ba
almax35 <almaximys@yandex.ru>
2023-05-29 20:27:46 +0300

    create department logic, add new dependencies to pom.xml

--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -2,19 +2,28 @@
 // update student
 // delete student
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department implements Publisher {
+
+    private final List<Subscriber> subscribers = new ArrayList<>();
+
     @Override
     public void notifySubscribers() {
-
+        for (Subscriber subscriber : subscribers) {
+            subscriber.update(this);
+        }
     }
 
     @Override
     public void addSubscriber(Subscriber subscriber) {
-
+        subscribers.add(subscriber);
     }
 
     @Override
     public void removeSubscriber(Subscriber subscriber) {
-
+        subscribers.remove(subscriber);
     }
 }
